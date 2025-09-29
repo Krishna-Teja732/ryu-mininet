459061d36eff9022fe461b422ca78f9c64e42172
Krishna Teja <b.krishnateja732@gmail.com>
2025-09-29 18:55:37 +0530

    Add scripts to get flow rules

--- scripts/run_ryu.sh
+++ scripts/run_ryu.sh
@@ -3,5 +3,4 @@
 # Run this script using bash. Do not run it directly
 
 
-ryu-manager ./controller/ryu_stp_controller_v2.py --wsapi-host=127.0.0.1 --wsapi-port=8090 --observe-links --ofp-tcp-listen-port 10001
-
+ryu-manager ./controller/ryu_stp_controller_v2.py ryu.app.rest_topology ryu.app.ofctl_rest --wsapi-host=127.0.0.1 --wsapi-port=8090 --observe-links --ofp-tcp-listen-port 10001 
