8de504483345b39b931674e483ce54d3b1ff6951
Krishna Teja <b.krishnateja732@gmail.com>
2026-01-28 11:27:35 +0530

    refactor: Make KGEventHandlerNoop write requests to event.txt

--- controller/kgevents/KGEventHandlerNoop.py
+++ controller/kgevents/KGEventHandlerNoop.py
@@ -2,28 +2,49 @@ import requests
 import requests.adapters
 from os_ken.topology.switches import Port, Switch, Link, Host
 
+url_base = "http://localhost:8080/ryu/openflow13"
 
 def send_switch_enter_event(dpid):
-    pass
+    print(f"Switch Enter: {dpid}")
+    with open("event.txt", "a") as file:
+        file.write(f"post,{url_base}/{dpid}\n")
+
 
 def send_switch_leave_event(dpid):
-    pass
+    print(f"Switch Leave: {dpid}")
+    with open("event.txt", "a") as file:
+        file.write(f"delete,{url_base}/{dpid}\n")
+
 
 def send_flow_add_event(dpid, table_id, request_body):
-    pass
+    print(f"Flow Add: {dpid}/{table_id} flowRule: {request_body}")
+    with open("event.txt", "a") as file:
+        file.write(f"post,{url_base}/{dpid}/{table_id}/flowrule,{request_body}\n")
 
 
 def send_flow_remove_event(dpid, table_id, request_body):
-    pass 
+    print(f"Flow remove: {dpid}/{table_id} flowRule: {request_body}")
+    with open("event.txt", "a") as file:
+        file.write(f"delete,{url_base}/{dpid}/{table_id}/flowrule,{request_body}\n")
 
 
 def send_link_add_event(request_body: dict):
-    pass
+    print(f"Link Add: {request_body}")
+    with open("event.txt", "a") as file:
+        file.write(f"post,{url_base}/links,{request_body}\n")
+
 
 def send_link_delete_event(request_body: dict):
-    pass
+    print(f"Link Delete: {request_body}")
+    with open("event.txt", "a") as file:
+        file.write(f"delete,{url_base}/links,{request_body}\n")
 
 
 def send_host_add_event(request_body: dict):
-    pass
+    print(f"Host add: {request_body}")
+    with open("event.txt", "a") as file:
+        file.write(f"post,{url_base}/hosts,{request_body}\n")
 
+
+def print_response(res: requests.Response):
+    print(res.request.method, res.url, res.status_code, res.request.body)
